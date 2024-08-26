9b1298f81a23c9900575b85609398b1e9bb9257d
Juan231299 <163695303+Juan231299@users.noreply.github.com>
2024-08-26 15:11:20 -0500

    Add files via upload

---
+++ src/TodoContext/useLocalStorage.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+function useLocalStorage(itemName, initialValue) {
+  const [item, setItem] = React.useState(initialValue);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(false);
+  
+  React.useEffect(() => {
+    setTimeout(() => {
+      try {
+        const localStorageItem = localStorage.getItem(itemName);
+    
+        let parsedItem;
+  
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
+          setItem(parsedItem);
+        }
+  
+        setLoading(false);
+      } catch(error) {
+        setLoading(false);
+        setError(true);
+      }
+    }, 2000);
+  });
+
+  const saveItem = (newItem) => {
+    localStorage.setItem(itemName, JSON.stringify(newItem));
+    setItem(newItem);
+  };
+
+  return {
+    item,
+    saveItem,
+    loading,
+    error,
+  };
+}
+
+export { useLocalStorage };
+
+
+/* localStorage.removeItem('TODOS_V1'); 
+
+const defaultTodos = [
+{ text: 'Cortar cebolla', completed: true },
+{ text: 'Cortar tomate', completed: false },
+{ text: 'Lavar lechuga', completed: false },
+{ text: 'Asar carne', completed: false },
+{ text: 'Armar hamburguesas', completed: true },
+];
+
+localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));  */
